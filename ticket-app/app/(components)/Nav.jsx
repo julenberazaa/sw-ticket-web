3ba5f652effe5558b92da21d8a4a43c8165f2be9
julenberazaa <julenberaza.ehu@gmail.com>
2024-05-05 13:12:59 +0200

    iniciamos el desarrollo 24

---
+++ ticket-app/app/(components)/Nav.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const Nav = () => {
+  return <nav>
+    <div>
+        <Link>
+            
+        </Link>
+    </div>
+    <div>
+        <p className="text-default-text">
+            julen@gmail.com
+        </p>
+    </div>
+  </nav>;
+};
+
+export default Nav;
